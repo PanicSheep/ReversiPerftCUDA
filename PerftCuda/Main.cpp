e810de3773a696a1a5ab28618440b98881a6c790
Dominic Hofer <dohofer86@gmail.com>
2021-01-11 21:19:11 +0100

    Updates to new file format and fixes testing bug.

--- PerftCuda/Main.cpp
+++ PerftCuda/Main.cpp
@@ -12,32 +12,6 @@
 #include <random>
 #include <map>
 
-void PrintHelp()
-{
-	std::cout
-		<< "This program calculates the number of possible Reversi games at the end of the n-th ply.\n"
-		<< "In correspondence with (https://oeis.org/A124004).\n\n"
-		<< "Args:\n"
-		<< " -f 'string'      File path.\n"
-		<< " -fd 'int'        Ply depth of positions in file.\n"
-		<< " -d 'int'         Ply depth to evaluate positions in file.\n"
-		<< " -ram 'string'    Size of the hash table in bytes (default: '1GB').\n"
-		<< " -t 'int' 'int'   Tests the results of position number 'int' to 'int', not including the latter.\n"
-		<< " -ta              Test all.\n"
-		<< " -cuda            Run on cuda capable GPUs.\n"
-		<< " -h               Prints this help.\n\n"
-		<< "Possible ways to use this program:\n"
-		<< " -f 'string' -fd 'int' -d 'int' (-cuda) (-ram 'int')\n"
-		<< "   Creates a file, if it does not exist, containing all possible Reversi positions as the end of the (fd)-th ply.\n"
-		<< "   It will open the file and calculate the number of possible Reversi games at the end of each position's (d)-th ply.\n"
-		<< "   When done it will sum up the intermediate results to conclude the number of possible Reversi positions at the end of the (fd+d)-th ply.\n"
-		<< " -f 'string' -fd 'int' -d 'int' -v 'int' 'int (-cuda) (-ram 'int')\n"
-		<< "   Tests the intermediate results in the file for correctness."
-		<< " -f 'string' -fd 'int' -d 'int' -va (-cuda) (-ram 'int')\n"
-		<< "   Tests all intermediate results for correctness."
-		<< std::endl;
-}
-
 #pragma pack(1)
 struct PosResDur
 {
@@ -65,13 +39,40 @@ std::vector<PosResDur> ReadFile(const std::filesystem::path& path)
 	return content;
 }
 
+void PrintHelp()
+{
+	std::cout
+		<< "This program calculates the number of possible Reversi games at the end of the n-th ply.\n"
+		<< "In correspondence with (https://oeis.org/A124004).\n\n"
+		<< "Args:\n"
+		<< " -f 'string'      File path.\n"
+		<< " -fd 'int'        Ply depth of positions in file.\n"
+		<< " -d 'int'         Ply depth to evaluate positions in file.\n"
+		<< " -ram 'string'    Size of the hash table in bytes (default: '1GB').\n"
+		<< " -t 'int' 'int'   Tests the results of position number 'int' to 'int', not including the latter.\n"
+		<< " -ta              Test all.\n"
+		<< " -cuda            Run on cuda capable GPUs.\n"
+		<< " -h               Prints this help.\n\n"
+		<< "Possible ways to use this program:\n"
+		<< " -f 'string' -fd 'int' -d 'int' (-cuda) (-ram 'int')\n"
+		<< "   Creates a file, if it does not exist, containing all possible Reversi positions as the end of the (fd)-th ply.\n"
+		<< "   It will open the file and calculate the number of possible Reversi games at the end of each position's (d)-th ply.\n"
+		<< "   When done it will sum up the intermediate results to conclude the number of possible Reversi positions at the end of the (fd+d)-th ply.\n"
+		<< " -f 'string' -fd 'int' -d 'int' -v 'int' 'int (-cuda) (-ram 'int')\n"
+		<< "   Tests the intermediate results in the file for correctness."
+		<< " -f 'string' -fd 'int' -d 'int' -va (-cuda) (-ram 'int')\n"
+		<< "   Tests all intermediate results for correctness."
+		<< std::endl;
+}
+
 int main(int argc, char* argv[])
 {
 	std::filesystem::path file;
-	int file_depth, depth, test_begin{0}, test_end{0};
-	uint64 RAM = 0;
+	uint file_depth, depth;
+	uint64 RAM{0}, test_begin{0}, test_end{0};
 	bool cuda = false;
 	bool test_all = false;
+	bool test = false;
 
 	for (int i = 0; i < argc; i++)
 	{
@@ -79,8 +80,8 @@ int main(int argc, char* argv[])
 		else if (std::string(argv[i]) == "-fd") file_depth = std::stoi(argv[++i]);
 		else if (std::string(argv[i]) == "-d") depth = std::stoi(argv[++i]);
 		else if (std::string(argv[i]) == "-ram") RAM = ParseBytes(argv[++i]);
-		else if (std::string(argv[i]) == "-t") { test_begin = std::stoi(argv[++i]); test_end = std::stoi(argv[++i]); }
-		else if (std::string(argv[i]) == "-ta") test_all = true;
+		else if (std::string(argv[i]) == "-t") { test_begin = std::stoi(argv[++i]); test_end = std::stoi(argv[++i]); test = true; }
+		else if (std::string(argv[i]) == "-ta") { test_all = true; test = true; }
 		else if (std::string(argv[i]) == "-cuda") cuda = true;
 		else if (std::string(argv[i]) == "-h") { PrintHelp(); return 0; }
 	}
@@ -93,8 +94,9 @@ int main(int argc, char* argv[])
 		<< "File depth: " << file_depth << "\n"
 		<< "Depth: " << depth << "\n"
 		<< "RAM: " << RAM << "\n"
+		<< "Testing: " << test << "\n"
 		<< "Testing all: " << test_all << "\n"
-		<< "Test range: " << test_begin << " to " << test_end << "\n"
+		<< "Range: " << test_begin << " to " << test_end << "\n"
 		<< "CUDA: " << cuda << std::endl;
 
 	std::unique_ptr<BasicPerft> engine;
@@ -106,6 +108,9 @@ int main(int argc, char* argv[])
 		engine = std::make_unique<UnrolledPerft>(6);
 
 	std::vector<PosResDur> work = ReadFile(file);
+	std::cout << "Positions in file: " << work.size() << "\n\n";
+	if (test_all)
+		test_end = work.size();
 
 	if (work.empty()) // file does not exist
 	{
@@ -115,46 +120,83 @@ int main(int argc, char* argv[])
 			work.push_back({FlipToUnique(pos), 0, 0.0});
 		std::sort(std::execution::par_unseq, work.begin(), work.end());
 		auto last = std::unique(std::execution::par_unseq, work.begin(), work.end());
-		work.erase(last, work.end());		
+		work.erase(last, work.end());
 		std::shuffle(work.begin(), work.end(), std::default_random_engine{});
 	}
 
-	std::cout << "Positions in file: " << work.size() << "\n\n";
-	std::cout << "   # |       Positions       |      Time[s]     |       Pos/s      | ETA\n";
-	std::cout << "-----+-----------------------+------------------+------------------+-----\n";
-
-	double total_duration = 0.0;
-	for (int i = 0; i < work.size(); i++)
+	if (test)
 	{
-		if (work[i].value == 0)
+		std::cout << "   # |       Positions       |correct|      Time[s]     |       Pos/s      | ETA \n";
+		std::cout << "-----+-----------------------+-------+------------------+------------------+-----\n";
+
+		double total_duration = 0.0;
+		for (std::size_t i = test_begin; i < test_end; i++)
 		{
 			const auto start = std::chrono::high_resolution_clock::now();
-			work[i].value = engine->calculate(work[i].pos, depth - file_depth);
-			const auto end = std::chrono::high_resolution_clock::now();
-			work[i].duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1'000.0;
-		}
-		total_duration += work[i].duration;
-		std::cout << std::setw(4) << i << " |";
-		std::cout << std::setw(22) << work[i].value << " |";
-		std::cout << std::setw(17) << work[i].duration << " | ";
-		if (work[i].duration)
-			std::cout << std::setw(17) << int64(work[i].value / work[i].duration)  << " | ";
-		auto t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now() + std::chrono::seconds(static_cast<long long>(total_duration / (i + 1) * (work.size() - i - 1))));
-		std::cout << std::ctime(&t);
+			const auto value = engine->calculate(work[i].pos, depth - file_depth);
+			const auto stop = std::chrono::high_resolution_clock::now();
+			work[i].duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() / 1'000.0;
 
-		WriteToFile(file, work);
+			total_duration += work[i].duration;
+			std::cout << std::setw(4) << i << " |";
+			std::cout << std::setw(22) << work[i].value << " |";
+			std::cout << std::setw(6) << (value == work[i].value) << " |";
+			std::cout << std::setw(17) << work[i].duration << " | ";
+			if (work[i].duration)
+				std::cout << std::setw(17) << int64(work[i].value / work[i].duration)  << " | ";
+			auto t = std::chrono::system_clock::to_time_t(
+				std::chrono::system_clock::now()
+				+ std::chrono::seconds(static_cast<long long>(
+					total_duration / (i - test_begin + 1) * (test_end - i - 1)
+					)));
+			std::cout << std::ctime(&t);
+
+			WriteToFile(file, work);
+		}
+	}
+	else
+	{
+		std::cout << "   # |       Positions       |      Time[s]     |       Pos/s      | ETA \n";
+		std::cout << "-----+-----------------------+------------------+------------------+-----\n";
+
+		double total_duration = 0.0;
+		for (std::size_t i = 0; i < work.size(); i++)
+		{
+			if (work[i].value == 0)
+			{
+				const auto start = std::chrono::high_resolution_clock::now();
+				work[i].value = engine->calculate(work[i].pos, depth - file_depth);
+				const auto stop = std::chrono::high_resolution_clock::now();
+				work[i].duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count() / 1'000.0;
+			}
+			total_duration += work[i].duration;
+			std::cout << std::setw(4) << i << " |";
+			std::cout << std::setw(22) << work[i].value << " |";
+			std::cout << std::setw(17) << work[i].duration << " | ";
+			if (work[i].duration)
+				std::cout << std::setw(17) << int64(work[i].value / work[i].duration)  << " | ";
+			auto t = std::chrono::system_clock::to_time_t(
+				std::chrono::system_clock::now()
+				+ std::chrono::seconds(static_cast<long long>(
+					total_duration / (i + 1) * (work.size() - i - 1)
+					)));
+			std::cout << std::ctime(&t);
+
+			WriteToFile(file, work);
+		}
+
+		std::map<Position, uint64> value;
+		for (const auto& w : work)
+			value[w.pos] = w.value;
+
+		// calculate final result
+		uint64 sum = 0;
+		for (const auto& pos : Children(Position::Start(), file_depth, true))
+			sum += value[FlipToUnique(pos)];
+		std::cout << "Sum: " << sum << std::endl;
+		std::cout << "Duration: " << total_duration << std::endl;
 	}
 
-	std::map<Position, uint64> value;
-	for (const auto& w : work)
-		value[w.pos] = w.value;
-
-	// calculate final result
-	uint64 sum = 0;
-	for (const auto& pos : Children(Position::Start(), file_depth, true))
-		sum += value[FlipToUnique(pos)];
-	std::cout << "Sum: " << sum << std::endl;
-	std::cout << "Duration: " << total_duration << std::endl;
 
 	if (cuda)
 	{
